6ac00399aa7a07d397c07a4bb120113b960f1962
fox <1792879730@qq.com>
2022-08-20 18:07:41 +0800

    maseter commit2

--- src/main/java/com/fox/git/GitTest.java
+++ src/main/java/com/fox/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("update");
         System.out.println("uodate hot-fix");
+        System.out.println("master test");
     }
 }
